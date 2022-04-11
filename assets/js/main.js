695ef7c5eabf68e89b082ab2783933380b9acaa4
atlazgit <oh.hi.atlaz@gmail.com>
2022-04-11 20:16:47 +0700

    update font dan menu , membuat scroll indicator percent

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,11 +12,26 @@ const glightbox = GLightbox({
   closeEffect: "fade",
 });
 
-const text = document.querySelector("article").innerText;
-function readingTime(text) {
-  const wpm = 200;
-  const words = text.trim().split(/\s+/).length;
-  const time = Math.ceil(words / wpm);
-  document.getElementById("time").innerText = time;
+const el = document.querySelector("article");
+if (el) {
+  const text = el && el.innerText;
+  function readingTime(text) {
+    const wpm = 200;
+    const words = text.trim().split(/\s+/).length;
+    const time = Math.ceil(words / wpm);
+    document.getElementById("time").innerText = time;
+  }
+  readingTime(text);
 }
-readingTime(text);
+
+// SCROLL INDICATOR
+const scrollIndicator = document.querySelector(".scroll-bar-indicator");
+function scrollProgress() {
+  const totalHeight = document.body.scrollHeight;
+  const currentDistance = document.documentElement.scrollTop;
+  const windowHeight = document.documentElement.clientHeight;
+  const percentScrolled =
+    (currentDistance / (totalHeight - windowHeight)) * 100;
+  scrollIndicator.style.width = Math.round(percentScrolled) + "%";
+}
+document.addEventListener("scroll", scrollProgress);
